ed0bbaebf36b452466cf2c561fe4e571086827c3
slighvan <56693892+slighvan@users.noreply.github.com>
2019-10-17 10:30:13 -0700

    initial template for project

---
+++ PD Fitness/PD Fitness/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PD Fitness
+//
+//  Created by Soroush Saheb-Pour Lighvan  on 2019-10-17.
+//  Copyright © 2019 Soroush Saheb-Pour Lighvan . All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PD Fitness/PD FitnessTests/PD_FitnessTests.swift
@@ -0,0 +1,34 @@
+//
+//  PD_FitnessTests.swift
+//  PD FitnessTests
+//
+//  Created by Soroush Saheb-Pour Lighvan  on 2019-10-17.
+//  Copyright © 2019 Soroush Saheb-Pour Lighvan . All rights reserved.
+//
+
+import XCTest
+@testable import PD_Fitness
+
+class PD_FitnessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
