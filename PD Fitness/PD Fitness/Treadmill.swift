0c2e9c93f3a067f8b9319efd300dc5503d3ab783
Soroush <slighvan@sfu.ca>
2019-11-14 21:50:52 -0800

    Added a lot more exercises

---
+++ PD Fitness/PD Fitness/Treadmill.swift
@@ -0,0 +1,30 @@
+//
+//  Treadmill.swift
+//  PD Fitness
+//
+//  Created by Soroush Saheb-Pour Lighvan  on 2019-11-14.
+//  Copyright © 2019 Soroush Saheb-Pour Lighvan . All rights reserved.
+//
+
+import UIKit
+
+class Treadmill: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
