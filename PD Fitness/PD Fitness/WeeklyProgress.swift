c50550d36b8f3c9c8b519232671ccb22cbef235e
Alexis Gervacio <lexigerv@gmail.com>
2019-11-23 11:39:01 -0800

    Added button and new view controller, version3 #1

---
+++ PD Fitness/PD Fitness/WeeklyProgress.swift
@@ -0,0 +1,30 @@
+//
+//  WeeklyProgress.swift
+//  PD Fitness
+//
+//  Created by Pam Gervacio on 2019-11-23.
+//  Copyright © 2019 Soroush Saheb-Pour Lighvan . All rights reserved.
+//
+
+import UIKit
+
+class WeeklyProgress: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
