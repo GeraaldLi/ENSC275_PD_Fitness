1c6875377252af6f9efe927577281cc229410779
System Administrator <root@d207-023-215-103.wireless.sfu.ca>
2019-12-01 12:45:39 -0800

    Added comments for weekly progress, changed backgroud color, version 3

--- PD Fitness/PD Fitness/WeeklyProgress.swift
+++ PD Fitness/PD Fitness/WeeklyProgress.swift
@@ -51,9 +51,6 @@ class WeeklyProgress: UIViewController {
     var dataEntries: [BarChartDataEntry] = []       //empty array for storing completed task data
     var dataEntries2: [BarChartDataEntry] = []      //empty array for storing total task data
     
-
-    
-    
     var pastDays: [Date?] = []
     
     override func viewDidLoad() {
@@ -70,15 +67,14 @@ class WeeklyProgress: UIViewController {
         if GIDSignIn.sharedInstance()!.currentUser != nil {
             googleUser = GIDSignIn.sharedInstance()!.currentUser
             userID = googleUser!.profile.name
-            print ("userId is", userID)
         }
         
+        //sets the formatting of date
         ref = Database.database().reference()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         
         let currentDate = Date()
-        
         var dateComponent = DateComponents()
             
         //loop for grabbing the completed task number and total task number
@@ -87,33 +83,30 @@ class WeeklyProgress: UIViewController {
         {
 
             let i = -j
+            //calculates the day from i days ago
             dateComponent.day = i
             pastDays.append(Calendar.current.date(byAdding: dateComponent, to: currentDate))
             dateFormString.append(dateFormatter.string(from: pastDays[j]!))
- 
             //
             //grabbing count of completed tasks
             completedTasksDbName.append("completedTasksDb" + dateFormString[j])
             completePath.append(rootDbPath + "/" + userID + "/" + dateFormString[j] + "/" + completedTasksDbName[j])
-            print(dateFormString[j])
             ref.child(completePath[j]).observeSingleEvent(of: .value, with: { snapshot in
                 if snapshot.hasChild("-Task_Count")
                 {
-                    print("---Random----")
+                    //if task count exists, then grabs the value into an array
                     self.ref.child(self.completePath[j]).child("-Task_Count").observeSingleEvent(of: .value, with: { (snapshot) in
                         let value = snapshot.value as! Double
                         
                         let dataEntry = BarChartDataEntry(x: Double(j), y: value)
                         self.dataEntries.insert(dataEntry, at:j)
-                        print("Adding ", value , "to Data Entry", "for date ", self.dateFormString[j])
                         })
                 }
                 else
                 {
+                    //sets task count to 0 if the child does not exist
                     let dataEntry = BarChartDataEntry(x: Double(j), y: 0)
                     self.dataEntries.insert(dataEntry, at:j)
-                    print("Adding 0 to Data Entry", "for date ", self.dateFormString[j])
-                  //  self.foo(index: 1)
                 }
 
                    })
@@ -128,8 +121,6 @@ class WeeklyProgress: UIViewController {
                     let value = snapshot.value as! Double
                     let dataEntry2 = BarChartDataEntry(x: Double(j), y: value)
                         self.dataEntries2.insert(dataEntry2, at:j)
-                        print("Adding ", value , "to Data Entry2", "for date ", self.dateFormString[j])
-                 //   self.foo(index: 1)
                         self.setChart2(Entries: self.dataEntries, Entries2: self.dataEntries2)
                     })
                 }
@@ -137,8 +128,6 @@ class WeeklyProgress: UIViewController {
                 {
                     let dataEntry2 = BarChartDataEntry(x: Double(j), y: 0)
                     self.dataEntries2.insert(dataEntry2, at:j)
-                    //self.foo(index:1)
-                    print("Adding 0 to Data Entry2")
                     self.setChart2(Entries: self.dataEntries, Entries2: self.dataEntries2)
                 }
                     })
@@ -150,11 +139,11 @@ class WeeklyProgress: UIViewController {
     
     func setChart2(Entries: [ChartDataEntry]?, Entries2: [ChartDataEntry]?)
     {
-        let chartDataSet = BarChartDataSet(entries: Entries, label: "Completed Tasks")
-        let chartDataSet1 = BarChartDataSet(entries: Entries2, label: "Total Tasks")
+        let chartDataSet = BarChartDataSet(entries: Entries, label: "Completed Tasks")  //creating a dataset for completed tasks
+        let chartDataSet1 = BarChartDataSet(entries: Entries2, label: "Total Tasks")    //creating a dataset for total tasks
 
 
-        let dataSets: [BarChartDataSet] = [chartDataSet,chartDataSet1]
+        let dataSets: [BarChartDataSet] = [chartDataSet,chartDataSet1]      //puttng both datasets in an array so they can be graphed together
         
         let chartData = BarChartData(dataSets: dataSets)
         
@@ -174,8 +163,8 @@ class WeeklyProgress: UIViewController {
         
         
         BarChartView.notifyDataSetChanged()
-        BarChartView.setVisibleXRangeMaximum(2)
-        BarChartView.moveViewToX(0)
+        BarChartView.setVisibleXRangeMaximum(2) //enables sliding on the chart view
+        BarChartView.moveViewToX(0)             //sets the page to open on the left side
         
         //Set BarChart Colour
         chartDataSet.colors = [UIColor(red: 100/255, green: 200/255, blue: 150/255, alpha: 1)]
@@ -184,40 +173,25 @@ class WeeklyProgress: UIViewController {
         BarChartView.data = chartData
     }
     
-    var months: [String]!
     
     func setChart(Date: [String])
     {
         //Chart Formatting
         BarChartView.chartDescription!.text = " "                       //removes description text
 
-        BarChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)       //background colour
-        BarChartView.animate(xAxisDuration: 2.5, yAxisDuration: 2.5, easingOption: .easeInQuart)
+        //BarChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)       //background colour
+        BarChartView.animate(xAxisDuration: 2.5, yAxisDuration: 2.5, easingOption: .easeInQuart)        //animates the bars when they appear
 
         
-        let xaxis = BarChartView.xAxis
+        let xaxis = BarChartView.xAxis          //shorter reference to the x-axis
         xaxis.drawGridLinesEnabled = true
         xaxis.centerAxisLabelsEnabled = true
-        xaxis.valueFormatter = IndexAxisValueFormatter(values:self.dateFormString)
-        let yaxis = BarChartView.leftAxis
+        xaxis.valueFormatter = IndexAxisValueFormatter(values:self.dateFormString)      //sets the x-axis names to the dates
+        let yaxis = BarChartView.leftAxis       //shorter reference to the y-axis
         yaxis.spaceTop = 0.35
         yaxis.axisMinimum = 0
         yaxis.drawGridLinesEnabled = false
         BarChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom               //sets x-axis labels to the bottom
-        
     }
-    private class BarChartFormatter: NSObject, IAxisValueFormatter {
-           
-           var values : [String]
-           required init (values : [String]) {
-               self.values = values
-               super.init()
-           }
     
-           func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-               return values[Int(value)]
-           }
-       }
-
-
 }
